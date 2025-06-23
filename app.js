c3f3f15d196a1611a009e952ed9015b39145a08b
AzizAmaanAhmed887 <amanahmed1122004@gmail.com>
2025-06-23 22:16:09 +0530

    new file added

---
+++ app.js
@@ -0,0 +1,7 @@
+MathOperations = (a, b) => {
+    console.log("Addition", (a + b));
+    console.log("Subtraction", (a - b));
+    console.log("Multiplication", (a * b));
+    console.log("Division", (a / b));
+};
+MathOperations(1, 2);
